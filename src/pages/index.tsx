b8cdebada691bc6c96e3945c10726b63b4046740
markus90souza <rockske8er@gmail.com>
2022-07-07 16:52:03 -0300

    chore(delete package not used): delete package not used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,10 @@
 import type { NextPage } from "next";
-import { Header } from "@components/Header";
-import { Hero } from "@components/Hero";
-import { BackToTop } from "@components/BackToTop";
-import { Projects } from "@components/Projects";
+
 
 const Home: NextPage = () => {
   return (
     <main className={"relative bg-white"}>
-      <Header />
-      <Hero />
-      <Projects />
-      <BackToTop />
+
     </main>
   );
 };
